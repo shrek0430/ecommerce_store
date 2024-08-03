ff9339d41ab861c95aeeadf66c2c35fcc8d8edf3
shrek0430 <206593158+shrek0430@users.noreply.github.com>
2024-08-04 04:21:49 +0530

    added build script

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,8 @@ process.on("uncaughtException", (err) => {
 //config
 dotenv.config({path:"backend/config/config.env"});
 
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
 //connecting to database
 connectDatabase();
 
